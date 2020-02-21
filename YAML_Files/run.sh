2a05b46a261a5782617cdecab3e1accd07501e59
dgeneve <dkgeneve@gmail.com>
2020-02-21 03:48:48 +0000

    2020-02-21_03:48:48

--- YAML_Files/run.sh
+++ YAML_Files/run.sh
@@ -1,12 +1,9 @@
 #/bin/bash
-if [ -z "$1" ]; then
-	echo "Usage: "
-	echo "./run.sh up"
-	echo "./run.sh up yaml_file.yaml"
-	echo "./run.sh down"
-	echo "./run.sh down yaml_file.yaml"
-	echo "./run.sh ps"
-	echo "./run.sh ps yaml_file.yaml"
+OPTION=$1
+main()
+{
+if [ -z "$OPTION" ]; then
+	show_help
 	exit
 fi
 case "$1" in
@@ -19,9 +16,25 @@ case "$1" in
 	ps)
 		DCMD=ps
 	;;
+	*)
+		echo "Invalid option"
+		show_help
+		exit
+	;;
 esac
 if [ ! -z "$2" ]; then
 	YAML_FILE="-f $2"
 fi
 sudo --preserve-env=PING_IDENTITY_DEVOPS_TAG,PING_IDENTITY_DEVOPS_USER,PING_IDENTITY_DEVOPS_KEY,PING_IDENTITY_DEVOPS_HOME /usr/local/bin/docker-compose $YAML_FILE $DCMD
-
+}
+show_help()
+{
+echo "Usage: "
+echo "./run.sh up"
+echo "./run.sh up yaml_file.yaml"
+echo "./run.sh down"
+echo "./run.sh down yaml_file.yaml"
+echo "./run.sh ps"
+echo "./run.sh ps yaml_file.yaml"
+}
+main
